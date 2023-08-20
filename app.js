7f454537740f58c3789984741e8fa3b1715f79d9
AvishayP <peravishaymoshe@gmail.com>
2023-08-20 10:32:33 +0300

    first add the morgan middlware

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import productsRouter from './api/products/routers.products.js';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 3000;
 
+// middleware for json the body of the request.
 app.use(express.json());
+// create "middleware"
+var logger = morgan('combined')
 
-app.use('/api/products', productsRoutes);
+
+app.use('/api/products', productsRouter);
 
 app.listen(PORT, () => {
     console.log('listening in port: ' + PORT);
